hanzoai/bot

fix(agents): classify structured unsupported model errors (#92280)
* fix(agents): classify structured unsupported model errors

* test(agents): update embedded harness helper mock

src/agents/embedded-agent-runner/run.ts
@@ -53,6 +53,7 @@ import {
 } from "../command/session.js";
 import { DEFAULT_MODEL, DEFAULT_PROVIDER } from "../defaults.js";
 import {
+  classifyAssistantFailoverReason,
   classifyFailoverReason,
   extractObservedOverflowTokenCount,
   type FailoverReason,
@@ -2797,12 +2798,7 @@ export async function runEmbeddedAgent(
           const rateLimitFailure = isRateLimitAssistantError(assistantForFailover);
           const billingFailure = isBillingAssistantError(assistantForFailover);
           const failoverFailure = isFailoverAssistantError(assistantForFailover);
-          const assistantFailoverReason = classifyFailoverReason(
-            assistantForFailover?.errorMessage ?? "",
-            {
-              provider: assistantForFailover?.provider,
-            },
-          );
+          const assistantFailoverReason = classifyAssistantFailoverReason(assistantForFailover);
           const assistantProviderStarted =
             Boolean(currentAttemptAssistant?.provider) ||
             idleTimedOut ||

ConorLynchOCGit/openclaw

src/agents/embedded-agent-runner/run.ts
@@ -64,6 +64,7 @@ import {
 } from "../command/session.js";
 import { DEFAULT_MODEL, DEFAULT_PROVIDER } from "../defaults.js";
 import {
+  classifyAssistantFailoverReason,
   classifyFailoverReason,
   extractObservedOverflowTokenCount,
   type FailoverReason,
@@ -2987,12 +2988,7 @@ async function runEmbeddedAgentInternal(
           const rateLimitFailure = isRateLimitAssistantError(attemptAssistant);
           const billingFailure = isBillingAssistantError(attemptAssistant);
           const failoverFailure = isFailoverAssistantError(attemptAssistant);
-          const assistantFailoverReason = classifyFailoverReason(
-            attemptAssistant?.errorMessage ?? "",
-            {
-              provider: attemptAssistant?.provider,
-            },
-          );
+          const assistantFailoverReason = classifyAssistantFailoverReason(attemptAssistant);
           const assistantProviderStarted =
             Boolean(currentAttemptAssistant?.provider) ||
             idleTimedOut ||